f95451a2c12eef26a4e595c642c85e1f52491d17
rzmsq <semin.02@mail.ru>
2025-09-03 17:32:55 +0300

    add comments

--- internal/timeformatter/formatter.go
+++ internal/timeformatter/formatter.go
@@ -1,3 +1,4 @@
+// Package timeformatter предоставляет функциональность для форматирования времени.
 package timeformatter
 
 import (
@@ -5,26 +6,32 @@ import (
 	"time"
 )
 
+// TimeFormatter интерфейс для форматирования времени в строку.
 type TimeFormatter interface {
+	// Format форматирует время согласно заданному формату.
 	Format(t time.Time) string
 }
 
+// defaultFormatter базовая реализация форматтера времени.
 type defaultFormatter struct {
-	layout string
+	layout string // шаблон форматирования времени
 }
 
-func NewDefaultFormatter(t time.Time) TimeFormatter {
+// NewDefaultFormatter создает форматтер с RFC3339 форматом по умолчанию.
+func NewDefaultFormatter() TimeFormatter {
 	return &defaultFormatter{
 		layout: time.RFC3339,
 	}
 }
 
+// NewCustomFormatter создает форматтер с пользовательским форматом.
 func NewCustomFormatter(layout string) TimeFormatter {
 	return &defaultFormatter{
 		layout: layout,
 	}
 }
 
+// Format возвращает отформатированное время с префиксом "Current time:".
 func (f *defaultFormatter) Format(t time.Time) string {
 	return fmt.Sprintf("Current time: %s", t.Format(f.layout))
 }
